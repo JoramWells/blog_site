8383e95a5558881dd8bc2081f4d5b094d59c18b4
bramuel <jorammanoah1@gmail.com>
2021-02-17 14:24:34 -0800

    Infinite Scroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Scroll from './components/Scroll'
+import Container from 'react-bootstrap/esm/Container'
+
 function App() {
   return (
     <>
-    <Button>
-      Hey
-    </Button>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">friend-Xone</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Updates</Nav.Link>
+      <Nav.Link href="#pricing">Add</Nav.Link>
+      <NavDropdown title="Categories" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Uhuru</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Oooliskia</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Tanga tanga</NavDropdown.Item>
+
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link eventKey={2} href="#memes">
+       joramWells@gmail.com
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+<Container>
+  <Scroll />
+</Container>
+
+
     </>
 
   );
---
+++ src/components/Scroll.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8,
+};
+
+class Scroll extends React.Component {
+  state = {
+    items: Array.from({ length: 20 }),
+  };
+
+  fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 })),
+      });
+    }, 1500);
+  };
+
+  render() {
+    return (
+      <div>
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          <div>
+            <Container fluid>
+            {this.state.items.map((i, index) => (
+              <Row key={index}>
+                
+                  <Col md={6} >
+                    <Card style={{ width: "100%", margin: "50px" }}>
+                      <Card.Img variant="top" src="holder.js/100px180" />
+                      <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                          Some quick example text to build on the card title and
+                          make up the bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="warning">Go somewhere</Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                
+              </Row>
+              ))}
+            </Container>
+          </div>
+        </InfiniteScroll>
+      </div>
+    );
+  }
+}
+
+export default Scroll;
